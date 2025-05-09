d1165168abebdfcbcfa5c51bce72b3894c9e7b3c
rahimuj570 <rahimujjaman2002@gmail.com>
2025-05-09 17:48:45 +0600

    q

--- assets/js/library_management.js
+++ assets/js/library_management.js
@@ -1,3 +1,5 @@
-setTimeout(function() {
-    window.location.href = "https://github.com/rahimuj570";
-}, 3000);
+window.onload = function() {
+    setTimeout(function() {
+        window.location.href = "https://github.com/rahimuj570";
+    }, 3000);
+};
